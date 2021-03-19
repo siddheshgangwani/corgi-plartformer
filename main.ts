9ee6bf7ec91ec4aadf9e56620a53ee685040a945
siddheshgangwani <79896587+siddheshgangwani@users.noreply.github.com>
2021-03-19 16:42:22 +0530

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     game.over(true, effects.bubbles)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    info.changeLifeBy(-2e-8)
+})
 scene.setBackgroundColor(9)
 let myCorg = corgio.create(SpriteKind.Player)
 myCorg.horizontalMovement()
@@ -8,3 +11,4 @@ myCorg.updateSprite()
 tiles.setTilemap(tilemap`level_3`)
 myCorg.follow()
 myCorg.verticalMovement()
+info.setLife(2)
